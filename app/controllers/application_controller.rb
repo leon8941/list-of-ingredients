f248d211fb4cae3ed10007bbc09e441a7392eb4e
Teck Onn <teckonn@wobb.co>
2018-09-28 16:28:41 +0800

    Add new home js and error handling functionality if page not found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
+    
+    def fallback_index_html
+        ender :file => 'public/index.html'
+    end
+    
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   scope '/api' do
     resources :drinks
   end
+  
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
 end
 
 
