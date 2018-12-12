52eee3a6589ce583cba021c05ff1a245ded99c82
Leon M <leon8941@gmail.com>
2018-12-13 00:22:53 +0800

    Remove files and start burger projects

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+class App extends Component {
+  render () {
+    return (
+      <div>
+        Welcome
+      </div>
+    )
+  }
+}
+
+export default App
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './containers/App'
+import App from './App';
 import 'semantic-ui-css/semantic.css';
 import './index.css';
 // import registerServiceWorker from './registerServiceWorker';
