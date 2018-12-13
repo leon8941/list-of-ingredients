f68696d21540364445c2cb4f9fd29910269ee167
Leon M <leon8941@gmail.com>
2018-12-13 17:36:21 +0800

    add build controls.

--- client/src/components/Burger/Burger.js
+++ client/src/components/Burger/Burger.js
@@ -4,14 +4,21 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(ingKey => {
             console.log("1st: ", ingKey);
             return [...Array(props.ingredients[ingKey])].map((_, i) => {
                 return <BurgerIngredient key={ingKey + i} type={ingKey} />;
             });
-        });
-
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, [])
+    
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please add some ingredients!</p>
+    }
+    
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
