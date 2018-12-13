f97e5327221e8ac46840b6547e29a7a53f15890f
Leon M <leon8941@gmail.com>
2018-12-13 11:16:45 +0800

    adding burger

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
-import React, { Component } from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
   render () {
     return (
       <div>
-        Welcome
+        <Layout>
+          <BurgerBuilder/>
+        </Layout>
       </div>
     )
   }
